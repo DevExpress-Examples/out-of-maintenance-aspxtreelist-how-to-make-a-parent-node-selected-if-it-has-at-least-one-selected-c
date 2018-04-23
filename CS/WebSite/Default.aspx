56eae2807978e2c05a9e0adeab33b73369db9fc1
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/WebSite/Default.aspx
@@ -0,0 +1,25 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<%@ Register Assembly="DevExpress.Web.ASPxTreeList.v12.2, Version=12.2.6.0, Culture=neutral, PublicKeyToken=b88d1754d700e49a" Namespace="DevExpress.Web.ASPxTreeList" TagPrefix="dx" %>
+<%@ Register Assembly="DevExpress.Web.v12.2, Version=12.2.6.0, Culture=neutral, PublicKeyToken=b88d1754d700e49a" Namespace="DevExpress.Web.ASPxEditors" TagPrefix="dx" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title> How to select a parent node if it has at least one child node selected</title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <dx:ASPxTreeList ID="treeList" runat="server" KeyFieldName="EmployeeID" ParentFieldName="ReportsTo" DataSourceID="SqlDataSource1"
+                OnDataBound="treeList_DataBound" OnSelectionChanged="treeList_SelectionChanged">
+                <SettingsSelection Enabled="true" />
+                <SettingsBehavior ProcessSelectionChangedOnServer="true" />
+            </dx:ASPxTreeList>
+        </div>
+        <asp:SqlDataSource ID="SqlDataSource1" runat="server" ConnectionString="<%$ ConnectionStrings:MyNorthwind %>"
+            SelectCommand="SELECT [EmployeeID], [LastName], [FirstName], [Title], [TitleOfCourtesy], [City], [Address], [ReportsTo] FROM [Employees]"></asp:SqlDataSource>
+    </form>
+</body>
+</html>
---
+++ CS/WebSite/Default.aspx.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using DevExpress.Web.ASPxTreeList;
+
+public partial class _Default : System.Web.UI.Page {
+    List<string> CurrentSelectedKeys;
+    string ChangedSelectionNodeKey;
+    List<string> afterRecursiveSelectionKeys;
+    protected List<string> AfterRecursiveSelectionKeys {
+        get {
+            if(afterRecursiveSelectionKeys == null) 
+                afterRecursiveSelectionKeys = Session["after"] as List<string>;
+            return afterRecursiveSelectionKeys;
+        }
+        set { Session["after"] = afterRecursiveSelectionKeys = value; }
+    }
+    protected void Page_Load(object sender, EventArgs e) {
+        if(!IsPostBack) Session.Clear();
+    }
+    protected void treeList_DataBound(object sender, EventArgs e) {
+        if(!IsPostBack) treeList.ExpandAll();
+    }
+    protected void treeList_SelectionChanged(object sender, EventArgs e) {
+        CurrentSelectedKeys = GetSelectedNodesKeys();
+        SetChangedSelectionNodeKey();
+        TreeListNode node = treeList.FindNodeByKeyValue(ChangedSelectionNodeKey);
+        SelectParentNodes(node);
+        SelectChildNodes(node);
+        AfterRecursiveSelectionKeys = GetSelectedNodesKeys();
+    }
+    List<string> GetSelectedNodesKeys() {
+        return treeList.GetSelectedNodes().Select(node => node.Key).ToList();
+    }
+    void SetChangedSelectionNodeKey() {
+        if(AfterRecursiveSelectionKeys == null)
+            ChangedSelectionNodeKey = CurrentSelectedKeys.First();
+        else {
+            ChangedSelectionNodeKey = AfterRecursiveSelectionKeys.Count > CurrentSelectedKeys.Count ?
+                AfterRecursiveSelectionKeys.Except(CurrentSelectedKeys).First() :
+                CurrentSelectedKeys.Except(AfterRecursiveSelectionKeys).First();
+        }
+    }
+    void SelectParentNodes(TreeListNode node) {
+        TreeListNode parent = node.ParentNode;
+        if(parent != null) {
+            if(node.Key != ChangedSelectionNodeKey)
+                node.Selected = !node.Selected;
+            if(!HasSelectedChildren(node, parent))
+                SelectParentNodes(parent);
+        }
+    }
+    bool HasSelectedChildren(TreeListNode node, TreeListNode parent) {
+        foreach(TreeListNode item in parent.ChildNodes) {
+            if(item.Selected && item.Key != node.Key) return true;
+        }
+        return false;
+    }
+    void SelectChildNodes(TreeListNode node) {
+        if(node.Key != ChangedSelectionNodeKey)
+            node.Selected = node.ParentNode.Selected ? !node.Selected : false;
+        foreach(TreeListNode item in node.ChildNodes) {
+            SelectChildNodes(item);
+        }
+    }
+}
